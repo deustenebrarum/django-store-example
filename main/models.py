a5d27f97140667322a029bf567ac2079faec53ee
deustenebrarum <gamesevolutionteam@gmail.com>
2024-03-18 21:18:29 +0300

    WIP

--- main/models.py
+++ main/models.py
@@ -20,6 +20,12 @@ class Product(models.Model):
     )
     count = models.IntegerField(default=0, verbose_name='Количество')
     is_active = models.BooleanField(default=True, verbose_name='Активен?')
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name='Дата создания'
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name='Дата обновления'
+    )
 
     class Meta:
         verbose_name = 'Товар'
