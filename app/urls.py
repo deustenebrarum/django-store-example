31f12fa87e8d61dbd99d0085f95ef2b9c0ed8687
deustenebrarum <gamesevolutionteam@gmail.com>
2024-03-25 19:49:00 +0300

    feat: Add ui kit button and replaced

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ from main.urls import urlpatterns as main_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(main_urls)),
+    path('auth/', include('django.contrib.auth.urls')),
 ] + static(MEDIA_URL, document_root=MEDIA_ROOT)
